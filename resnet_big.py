c757210233516eaf76c07163d38ed9fe40f69ab1
Hong Nguyen <49111748+Hong7Cong@users.noreply.github.com>
2025-04-03 22:18:59 -0700

    Add files via upload

---
+++ resnet_big.py
@@ -0,0 +1,428 @@
+"""ResNet in PyTorch.
+ImageNet-Style ResNet
+[1] Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun
+    Deep Residual Learning for Image Recognition. arXiv:1512.03385
+Adapted from: https://github.com/bearpaw/pytorch-classification
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+from einops import rearrange
+from torch import einsum
+
+from itertools import combinations, product
+# import torch
+
+def sets_to_tensor(list_of_sets):
+    return torch.tensor([list(s) for s in list_of_sets], dtype=torch.float32)
+# def pairwise_combinations(lst):
+#     return list(combinations(lst, 2))
+
+def pairwise_combinations_tensor(lst):
+    pairs = list(product(lst, repeat=2))
+    pairs = [(p1.tolist(), p2.tolist()) for p1, p2 in pairs]  # Convert tensors to lists
+    return torch.tensor(pairs)
+
+def exists(val):
+    return val is not None
+
+def default(val, d):
+    return val if exists(val) else d
+
+# bidirectional cross attention - have two sequences attend to each other with 1 attention step
+
+class BidirectionalCrossAttention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        heads = 8,
+        dim_head = 64,
+        context_dim = None,
+        dropout = 0.,
+        talking_heads = False,
+        prenorm = False
+    ):
+        super().__init__()
+        context_dim = default(context_dim, dim)
+
+        self.norm = nn.LayerNorm(dim) if prenorm else nn.Identity()
+        self.context_norm = nn.LayerNorm(context_dim) if prenorm else nn.Identity()
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        inner_dim = dim_head * heads
+
+        self.dropout = nn.Dropout(dropout)
+        self.context_dropout = nn.Dropout(dropout)
+
+        self.to_qk = nn.Linear(dim, inner_dim, bias = False)
+        self.context_to_qk = nn.Linear(context_dim, inner_dim, bias = False)
+
+        self.to_v = nn.Linear(dim, inner_dim, bias = False)
+        self.context_to_v = nn.Linear(context_dim, inner_dim, bias = False)
+
+        self.to_out = nn.Linear(inner_dim, dim)
+        self.context_to_out = nn.Linear(inner_dim, context_dim)
+
+        self.talking_heads = nn.Conv2d(heads, heads, 1, bias = False) if talking_heads else nn.Identity()
+        self.context_talking_heads = nn.Conv2d(heads, heads, 1, bias = False) if talking_heads else nn.Identity()
+
+    def forward(
+        self,
+        x,
+        context,
+        mask = None,
+        context_mask = None,
+        return_attn = False,
+        rel_pos_bias = None
+    ):
+        b, i, j, h, device = x.shape[0], x.shape[-2], context.shape[-2], self.heads, x.device
+
+        
+        x = self.norm(x)
+        context = self.context_norm(context)
+
+        # get shared query/keys and values for sequence and context
+
+        qk, v = self.to_qk(x), self.to_v(x)
+
+        context_qk, context_v = self.context_to_qk(context), self.context_to_v(context)
+
+  
+        # split out head
+
+        qk, context_qk, v, context_v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), (qk, context_qk, v, context_v))
+
+        # get similarities
+
+        sim = einsum('b h i d, b h j d -> b h i j', qk, context_qk) * self.scale
+
+        # relative positional bias, if supplied
+
+        if exists(rel_pos_bias):
+            sim = sim + rel_pos_bias
+
+        # mask
+
+        if exists(mask) or exists(context_mask):
+            mask = default(mask, torch.ones((b, i), device = device, dtype = torch.bool))
+            context_mask = default(context_mask, torch.ones((b, j), device = device, dtype = torch.bool))
+
+            attn_mask = rearrange(mask, 'b i -> b 1 i 1') * rearrange(context_mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~attn_mask, -torch.finfo(sim.dtype).max)
+
+        # get attention along both sequence length and context length dimensions
+        # shared similarity matrix
+
+        attn = sim.softmax(dim = -1)
+        context_attn = sim.softmax(dim = -2)
+
+        # dropouts
+
+        attn = self.dropout(attn)
+        context_attn = self.context_dropout(context_attn)
+
+        # talking heads
+
+        attn = self.talking_heads(attn)
+        context_attn = self.context_talking_heads(context_attn)
+
+        # src sequence aggregates values from context, context aggregates values from src sequence
+
+        out = einsum('b h i j, b h j d -> b h i d', attn, context_v)
+        context_out = einsum('b h j i, b h j d -> b h i d', context_attn, v)
+
+        # merge heads and combine out
+
+        out, context_out = map(lambda t: rearrange(t, 'b h n d -> b n (h d)'), (out, context_out))
+
+        out = self.to_out(out)
+        context_out = self.context_to_out(context_out)
+
+        if return_attn:
+            return out, context_out, attn, context_attn
+
+        return out, context_out
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, is_last=False):
+        super(BasicBlock, self).__init__()
+        self.is_last = is_last
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        preact = out
+        out = F.relu(out)
+        if self.is_last:
+            return out, preact
+        else:
+            return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1, is_last=False):
+        super(Bottleneck, self).__init__()
+        self.is_last = is_last
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        preact = out
+        out = F.relu(out)
+        if self.is_last:
+            return out, preact
+        else:
+            return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, in_channel=3, zero_init_residual=False):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(in_channel, 64, kernel_size=3, stride=1, padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves
+        # like an identity. This improves the model by 0.2~0.3% according to:
+        # https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for i in range(num_blocks):
+            stride = strides[i]
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x, layer=100):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        return out
+
+
+def resnet18(**kwargs):
+    return ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
+
+
+def resnet34(**kwargs):
+    return ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
+
+
+def resnet50(**kwargs):
+    return ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
+
+
+def resnet101(**kwargs):
+    return ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
+    
+model_dict = {
+    'resnet18': [resnet18, 512],
+    'resnet34': [resnet34, 512],
+    'resnet50': [resnet50, 2048],
+    'resnet101': [resnet101, 2048],
+    'vit_small': [torch.hub.load('facebookresearch/dino:main', 'dino_vits16'), 384],
+    'vit_s_dino': [torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14_reg'), 384],
+    # 'vit_b_dino': [torch.hub.load('facebookresearch/dinov2', 'dinov2_vitb14_reg'), 768],
+    # 'vit_l_dino': [torch.hub.load('facebookresearch/dinov2', 'dinov2_vitl14_reg'), 1024],
+    # 'vit_g_dino': [torch.hub.load('facebookresearch/dinov2', 'dinov2_vitg14_reg'), 1536],
+    'vit_base': [models.vision_transformer.vit_b_16, 768], 
+    'vit_large': [models.vision_transformer.vit_l_16, 1024] 
+}
+
+
+class LinearBatchNorm(nn.Module):
+    """Implements BatchNorm1d by BatchNorm2d, for SyncBN purpose"""
+    def __init__(self, dim, affine=True):
+        super(LinearBatchNorm, self).__init__()
+        self.dim = dim
+        self.bn = nn.BatchNorm2d(dim, affine=affine)
+
+    def forward(self, x):
+        x = x.view(-1, self.dim, 1, 1)
+        x = self.bn(x)
+        x = x.view(-1, self.dim)
+        return x
+
+class CrossAttViT(nn.Module):
+    def __init__(self, name='vit_small', head='mlp', feat_dim=128, method="SupCon"):
+        super(CrossAttViT, self).__init__()
+        model, dim_in = model_dict['vit_small']  
+        self.encoder = model.cuda()
+
+        self.encoder.heads = nn.Identity() 
+        self.cross_atten = BidirectionalCrossAttention(
+            dim = dim_in,  
+            heads = 8,
+            dim_head = 64,
+            context_dim = dim_in
+        )
+        
+        if head == 'linear':
+            self.head = nn.Linear(dim_in, feat_dim)
+        elif head == 'mlp':
+            self.head = nn.Sequential(
+                nn.Linear(dim_in, dim_in),
+                nn.ReLU(inplace=True),
+                nn.Linear(dim_in, feat_dim)
+            )
+        
+    def forward(self, images):
+        bsz = images.shape[0]
+        pairs = pairwise_combinations_tensor(images).cuda()
+        # inputs = sets_to_tensor(pairs)
+        # print(pairs.device, self.encoder.device)
+        # assert False
+        
+        img1_embs = self.encoder.get_intermediate_layers(pairs[:,0,:], n=1)[0]
+        img2_embs = self.encoder.get_intermediate_layers(pairs[:,1,:], n=1)[0]
+        # img1_emb, img2_emb = torch.split(imgs_emb, [bsz, bsz], dim=0)
+                    
+        img1_att, img2_att = self.cross_atten(img1_embs, img2_embs, mask=None, context_mask=None)
+        # print(img1_att.shape, img1_embs.shape)
+        # print(pairdot.shape)
+        # assert False
+        feat_img1 = F.normalize(self.head(img1_att), dim=2, eps=1e-8)
+        feat_img2 = F.normalize(self.head(img2_att), dim=2, eps=1e-8)
+
+        pairdot = torch.sum(torch.mul(feat_img1[:,0,:], feat_img2[:,0,:]), dim=1).view(bsz, bsz)
+
+        return pairdot
+
+        
+        
+        #loss = sum f diagnosis + sum (1-i)
+        
+
+class SupConViT(nn.Module):
+    def __init__(self, name='vit_base', head='mlp', feat_dim=128):
+        super(SupConViT, self).__init__()
+        model_fun, dim_in = model_dict[name]
+        if 'dino' in name:
+            self.encoder = model_fun  
+        else:
+            self.encoder = model_fun(pretrained=True)
+
+        self.encoder.heads = nn.Identity()  
+        if head == 'linear':
+            self.head = nn.Linear(dim_in, feat_dim)
+        elif head == 'mlp':
+            self.head = nn.Sequential(
+                nn.Linear(dim_in, dim_in),
+                nn.ReLU(inplace=True),
+                nn.Linear(dim_in, feat_dim)
+            )
+        else:
+            raise NotImplementedError('head not supported: {}'.format(head))
+
+    def forward(self, x):
+        feat = self.encoder(x) 
+        feat = F.normalize(self.head(feat), dim=1) 
+        return feat
+
+class SupConResNet(nn.Module):
+    """backbone + projection head"""
+    def __init__(self, name='resnet50', head='mlp', feat_dim=128):
+        super(SupConResNet, self).__init__()
+        model_fun, dim_in = model_dict[name]
+        self.encoder = model_fun()
+        if head == 'linear':
+            self.head = nn.Linear(dim_in, feat_dim)
+        elif head == 'mlp':
+            self.head = nn.Sequential(
+                nn.Linear(dim_in, dim_in),
+                nn.ReLU(inplace=True),
+                nn.Linear(dim_in, feat_dim)
+            )
+        else:
+            raise NotImplementedError(
+                'head not supported: {}'.format(head))
+
+    def forward(self, x):
+        feat = self.encoder(x)
+        feat = F.normalize(self.head(feat), dim=1)
+        return feat
+
+
+class SupCEResNet(nn.Module):
+    """encoder + classifier"""
+    def __init__(self, name='resnet50', num_classes=10):
+        super(SupCEResNet, self).__init__()
+        model_fun, dim_in = model_dict[name]
+        self.encoder = model_fun()
+        self.fc = nn.Linear(dim_in, num_classes)
+
+    def forward(self, x):
+        return self.fc(self.encoder(x))
+
+
+class LinearClassifier(nn.Module):
+    """Linear classifier"""
+    def __init__(self, name='resnet50', num_classes=10):
+        super(LinearClassifier, self).__init__()
+        _, feat_dim = model_dict[name]
+        self.fc = nn.Linear(feat_dim, num_classes)
+
+    def forward(self, features):
+        return self.fc(features)
